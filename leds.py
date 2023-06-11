21380adad63d39d7b1e70156f9a6a09827d452e4
JohannesWM <97265854+JohannesWM@users.noreply.github.com>
2023-06-10 23:57:21 -0400

    web part of everything works, however still need to do led file

--- leds.py
+++ leds.py
@@ -56,8 +56,6 @@ def rainbow_check():
 def rainbow_cycle(wait):
     for j in range(255):
         for i in range(num_pixels):
-            if rainbow_check() is True:
-                break
             pixel_index = (i * 256 // num_pixels) + j
             pixels[i] = wheel(pixel_index & 255)
         pixels.show()
