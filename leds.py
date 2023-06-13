684fe5a8760062bd3aa9dd19ca84e06e71ebe823
JohannesWM <97265854+JohannesWM@users.noreply.github.com>
2023-06-13 17:47:50 -0400

    web part of everything works, however still need to do led file

--- leds.py
+++ leds.py
@@ -26,7 +26,7 @@ num_pixels = 100
 ORDER = neopixel.GRB
 
 pixels = neopixel.NeoPixel(
-    pixel_pin, num_pixels, brightness=0.8, auto_write=False, pixel_order=ORDER
+    pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
 )
 
 
