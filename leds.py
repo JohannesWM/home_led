d7b4734295c7472f77f4e64e1f3d8b11d4c1c8f9
JohannesWM <97265854+JohannesWM@users.noreply.github.com>
2023-06-15 19:24:11 -0400

    web part of everything works, however still need to do led file

--- leds.py
+++ leds.py
@@ -98,6 +98,39 @@ def racer(length=10, color=colors["Purple"], delay_time=0.001):
         time.sleep(delay_time)
 
 
+def speed_racer(length=10, color=colors["Purple"], delay_time=0.001):
+    current_length = 0
+    for led in range(num_pixels):
+        delay_time -= 0.0001
+
+        if modeFunctions.get_current_mode() != "toggleRacer":
+            break
+
+        if current_length < length:
+            current_length += 1
+            pixels[led] = color
+
+        elif led == 99:
+
+            pixels[led] = color
+            time.sleep(delay_time)
+
+            for subled in range(current_length):
+                pixels[led - current_length + subled] = colors["Black"]
+                pixels.show()
+                time.sleep(delay_time)
+
+            pixels[99] = colors["Black"]
+            pixels.show()
+
+        elif current_length == length:
+            pixels[led] = color
+            pixels[led - current_length] = colors["Black"]
+
+        pixels.show()
+        time.sleep(delay_time)
+
+
 def lights_server_link():
     while True:
         try:
@@ -112,8 +145,7 @@ def lights_server_link():
                 print("toggleTimer60")
                 time.sleep(1)
             elif modeFunctions.get_current_mode() == "toggleSpeedRacer":
-                print("toggleSpeedRacer")
-                time.sleep(1)
+                speed_racer()
             else:
                 print("ERROR")
 
